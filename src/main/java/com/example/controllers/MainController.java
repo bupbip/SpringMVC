7a4b71fc13f86417f3cf72425b2777eeb1cc676c
peitos <andryhoy@gmail.com>
2022-06-11 18:53:03 +0300

    add registration, reload to ftlh from mustache(macroses), add info about author message

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,10 +1,11 @@
 package com.example.controllers;
 
 import com.example.domain.Message;
+import com.example.domain.User;
 import com.example.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
@@ -24,29 +25,40 @@ public class GreetingController {
     @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
+
         model.put("messages", messages);
+
         return "main";
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
+
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
+
         model.put("messages", messages);
+
         return "main";
     }
 
     @PostMapping("filter")
-    public String filter(@RequestParam String text, Map<String, Object> model) {
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
         Iterable<Message> messages;
-        if (!text.isEmpty() && text != null) {
-            messages = messageRepo.findByTag(text);
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
         } else {
             messages = messageRepo.findAll();
         }
+
         model.put("messages", messages);
+
         return "main";
     }
+
 }
--- src/main/java/com/example/domain/Message.java
+++ src/main/java/com/example/domain/Message.java
@@ -1,20 +1,16 @@
 package com.example.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Message {
 
-    public Message() {
-    }
-
-    public Message(String text, String tag) {
-        this.text = text;
-        this.tag = tag;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,6 +18,40 @@ public class Message {
 
     private String text;
     private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
 
     public String getText() {
         return text;
