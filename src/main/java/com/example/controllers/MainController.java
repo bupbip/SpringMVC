60edb0389b0d03ac0847f9af73cd20f91f8ea801
peitos <andryhoy@gmail.com>
2022-06-15 12:41:52 +0300

    пофикшен баг, при котором после поиска по фильтру нельзя было написать новое сообщение

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -23,10 +23,15 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> messages = messageRepo.findAll();
-
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
         model.put("messages", messages);
+        model.put("filter", filter);
 
         return "main";
     }
@@ -46,19 +51,19 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
+//    @PostMapping("filter")
+//    public String filter(@RequestParam String filter, Map<String, Object> model) {
+//        Iterable<Message> messages;
+//
+//        if (filter != null && !filter.isEmpty()) {
+//            messages = messageRepo.findByTag(filter);
+//        } else {
+//            messages = messageRepo.findAll();
+//        }
+//
+//        model.put("messages", messages);
+//
+//        return "main";
+//    }
 
 }
