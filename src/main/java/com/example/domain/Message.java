0259e6e310e1d5927cd4e24d572a8266c2046b20
peitos <andryhoy@gmail.com>
2022-06-15 22:53:16 +0300

    Навёл красоту

--- src/main/java/com/example/domain/Message.java
+++ src/main/java/com/example/domain/Message.java
@@ -23,6 +23,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message() {
     }
 
@@ -67,5 +69,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
